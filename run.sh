070a6a7804059087af5b082a1305a985a175cf4a
Michal Svorc <michal@svorc.sk>
2020-01-19 19:01:27 +0100

    Remove empty host mount directories, add mkdir to run.sh script

--- run.sh
+++ run.sh
@@ -2,12 +2,16 @@
 # Author: Michal Svorc <michalsvorc.com>
 # Run docker container with predefined mount directories
 
-# Declare variables
+# Image variables
 image_repository='michalsvorc'
 image_name='inkscape'
 image_tag=$(git describe --tags --abbrev=0)
 user_name=$image_name
+
+# Mount variables
 mount_path="${PWD}"
+dir_workspace_host="${mount_path}/workspace"
+dir_workspace_container="/home/$user_name/workspace"
 
 # Run
 docker run \
@@ -15,6 +19,6 @@ docker run \
     --rm \
     --env DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --mount type=bind,source=$mount_path/workspace,target=/home/$user_name/workspace \
+    --mount type=bind,source=$dir_workspace_host,target=$dir_workspace_container \
     --name $image_repository-$image_name-$image_tag \
     $image_repository/$image_name:$image_tag
