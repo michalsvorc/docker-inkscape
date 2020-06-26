0b1ff6346ab307c04c96e8e2f04f87f504c4b62a
Michal Svorc <mail@michalsvorc.com>
2020-06-26 16:10:08 +0200

    Docker run command formatting

--- docker.sh
+++ docker.sh
@@ -72,12 +72,12 @@ _docker_run() {
     docker run \
         -it \
         --rm \
-        --env DISPLAY=$DISPLAY \
-        -v /tmp/.X11-unix:/tmp/.X11-unix \
-        --mount type=bind,source=$mount_config_source,target=$mount_config_target \
-        --mount type=bind,source=$mount_workspace_source,target=$mount_workspace_target \
-        --name $container_name \
-        $image_name:$image_tag
+        --env DISPLAY="${DISPLAY}" \
+        -v '/tmp/.X11-unix:/tmp/.X11-unix' \
+        --mount type=bind,source="${mount_config_source}",target="${mount_config_target}" \
+        --mount type=bind,source="${mount_workspace_source}",target="${mount_workspace_target}" \
+        --name "${container_name}" \
+        "${image_name}:${image_tag}"
 }
 
 #===============================================================================
